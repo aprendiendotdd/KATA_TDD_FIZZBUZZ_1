445ab202ca18400123b025df1c4ba7633a538865
aprendiendotdd <aprendiendotdd@outlook.com>
2013-10-27 21:28:58 +0100

    Refactorizando Test

--- KataFizzBuzzTests/KataFizzBuzzTests.cs
+++ KataFizzBuzzTests/KataFizzBuzzTests.cs
@@ -2,156 +2,70 @@
 
 namespace KataFizzBuzzTests
 {
-    public class KataFizzBuzzTests
+  [TestFixture]
+  public class KataFizzBuzzTests
+  {
+
+
+    [Test]
+    public void Pasamos_Numero_Uno_Debe_Devolver_Uno()
     {
-        [TestFixture]
-        public class Paso_Numero_Uno_Al_Juego {
-            KataFizzBuzz.IKataFizzBuzz SUT = new KataFizzBuzz.KataFizzBuzz();
-            private string resultado;
-
-            [SetUp]
-            public void Setup() {
-                resultado = SUT.Game("1");
-            }
-
-            [Test]
-            public void Debe_Devolver_Uno() {
-                Assert.AreEqual(resultado, "1");
-            }
-        }
-
-        [TestFixture]
-        public class Paso_Numero_Dos_Al_Juego
-        {
-            KataFizzBuzz.IKataFizzBuzz SUT = new KataFizzBuzz.KataFizzBuzz();
-            private string resultado;
-
-            [SetUp]
-            public void Setup()
-            {
-                resultado = SUT.Game("2");
-            }
-
-            [Test]
-            public void Debe_Devolver_Dos()
-            {
-                Assert.AreEqual(resultado, "2");
-            }
-        }
-
-        [TestFixture]
-        public class Paso_Numero_Divisible_Tres_Al_Juego
-        {
-            KataFizzBuzz.IKataFizzBuzz SUT = new KataFizzBuzz.KataFizzBuzz();
-            private string resultado;
-
-            [SetUp]
-            public void Setup()
-            {
-                resultado = SUT.Game("3");
-            }
-
-            [Test]
-            public void Debe_Devolver_Fizz()
-            {
-                Assert.AreEqual(resultado, "Fizz");
-            }
-        }
-
-        [TestFixture]
-        public class Paso_Numero_Divisible_Cinco_Al_Juego
-        {
-            KataFizzBuzz.IKataFizzBuzz SUT = new KataFizzBuzz.KataFizzBuzz();
-            private string resultado;
-
-            [SetUp]
-            public void Setup()
-            {
-                resultado = SUT.Game("5");
-            }
-
-            [Test]
-            public void Debe_Devolver_Buzz()
-            {
-                Assert.AreEqual(resultado, "Buzz");
-            }
-        }
-
-        [TestFixture]
-        public class Paso_Numero_Divisible_Tres_Y_Cinco_Al_Juego
-        {
-            KataFizzBuzz.IKataFizzBuzz SUT = new KataFizzBuzz.KataFizzBuzz();
-            private string resultado;
-
-            [SetUp]
-            public void Setup()
-            {
-                resultado = SUT.Game("60");
-            }
-
-            [Test]
-            public void Debe_Devolver_FizzBuzz()
-            {
-                Assert.AreEqual(resultado, "FizzBuzz");
-            }
-        }
-
-        [TestFixture]
-        public class Paso_Numero_Que_Contiene_Tres_Pero_No_Es_Divisible_Tres
-        {
-            KataFizzBuzz.IKataFizzBuzz SUT = new KataFizzBuzz.KataFizzBuzz();
-            private string resultado;
-
-            [SetUp]
-            public void Setup()
-            {
-                resultado = SUT.Game("13");
-            }
-
-            [Test]
-            public void Debe_Devolver_Fizz()
-            {
-                Assert.AreEqual(resultado, "Fizz");
-            }
-        }
-
-        [TestFixture]
-        public class Paso_Numero_Que_Contiene_Cinco_Pero_No_Es_Divisible_Cinco
-        {
-            KataFizzBuzz.IKataFizzBuzz SUT = new KataFizzBuzz.KataFizzBuzz();
-            private string resultado;
-
-            [SetUp]
-            public void Setup()
-            {
-                resultado = SUT.Game("52");
-            }
-
-            [Test]
-            public void Debe_Devolver_Buzz()
-            {
-                Assert.AreEqual(resultado, "Buzz");
-            }
-        }
-
-        [TestFixture]
-        public class Paso_Numero_Que_Contiene_Tres_Y_Cinco
-        {
-            KataFizzBuzz.IKataFizzBuzz SUT = new KataFizzBuzz.KataFizzBuzz();
-            private string resultado;
-
-            [SetUp]
-            public void Setup()
-            {
-                resultado = SUT.Game("53");
-            }
-
-            [Test]
-            public void Debe_Devolver_FizzBuzz()
-            {
-                Assert.AreEqual(resultado, "FizzBuzz");
-            }
-        }
-        
+      var iKataFizzBuzz = new KataFizzBuzz.KataFizzBuzz();
+      Assert.AreEqual("1", iKataFizzBuzz.Game("1"));
     }
+
+    [Test]
+    public void Pasamos_Numero_Dos_Debe_Devolver_Dos()
+    {
+      var iKataFizzBuzz = new KataFizzBuzz.KataFizzBuzz();
+      Assert.AreEqual("2", iKataFizzBuzz.Game("2"));
+    }
+
+    [Test]
+    public void Pasamos_Numero_Divisible_Tres_Al_Juego_Debe_Devolver_Fizz()
+    {
+      var iKataFizzBuzz = new KataFizzBuzz.KataFizzBuzz();
+      Assert.AreEqual("Fizz", iKataFizzBuzz.Game("3"));
+      Assert.AreEqual("Fizz", iKataFizzBuzz.Game("6"));
+    }
+
+    [Test]
+    public void Pasamos_Numero_Divisible_Cinco_Al_Juego_Debe_Devolver_Buzz()
+    {
+      var iKataFizzBuzz = new KataFizzBuzz.KataFizzBuzz();
+      Assert.AreEqual("Buzz", iKataFizzBuzz.Game("5"));
+      Assert.AreEqual("Buzz", iKataFizzBuzz.Game("10"));
+    }
+
+    [Test]
+    public void Pasamos_Numero_Divisible_Tres_Cinco_Al_Juego_Debe_Devolver_FizzBuzz()
+    {
+      var iKataFizzBuzz = new KataFizzBuzz.KataFizzBuzz();
+      Assert.AreEqual("FizzBuzz", iKataFizzBuzz.Game("60"));
+      Assert.AreEqual("FizzBuzz", iKataFizzBuzz.Game("15"));
+    }
+
+    [Test]
+    public void Pasamos_Numero_Que_Contiene_Tres_Pero_No_Es_Divisible_Tres_Debe_Devolver_Fizz()
+    {
+      var iKataFizzBuzz = new KataFizzBuzz.KataFizzBuzz();
+      Assert.AreEqual("Fizz", iKataFizzBuzz.Game("23"));
+      Assert.AreEqual("Fizz", iKataFizzBuzz.Game("13"));
+    }
+
+    [Test]
+    public void Pasamos_Numero_Que_Contiene_Cinco_Pero_No_Es_Divisible_Tres_Debe_Devolver_Buzz()
+    {
+      var iKataFizzBuzz = new KataFizzBuzz.KataFizzBuzz();
+      Assert.AreEqual("Buzz", iKataFizzBuzz.Game("52"));
+    }
+
+    [Test]
+    public void Pasamos_Numero_Que_Contiene_Tres_Y_Cinco_Debe_Devolver_FizzBuzz()
+    {
+      var iKataFizzBuzz = new KataFizzBuzz.KataFizzBuzz();
+      Assert.AreEqual("FizzBuzz", iKataFizzBuzz.Game("53"));
+    }
+
+  }
 }
